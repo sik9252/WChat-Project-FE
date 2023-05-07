5306e77d92b282ac3da4218262ef5c1a3573c0af
sik9252 <sik9252@naver.com>
2023-05-07 23:01:49 +0900

    chore: react-query 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>,
 );
