94e87f47150be7aaadb07133d544782d3e5eaa2a
sik9252 <sik9252@naver.com>
2023-05-08 06:00:03 +0900

    feat: 닉네임 변경 API 추가

--- src/utils/axios/MyInfoApi/index.js
+++ src/utils/axios/MyInfoApi/index.js
@@ -4,3 +4,8 @@ import { useAxios } from '../useAxios';
 export const getMyInfoReq = async () => {
   return await useAxios.get(`/auth/info`);
 };
+
+/** 내 닉네임 변경 */
+export const changeMyNickReq = async (data) => {
+  return await useAxios.put(`/auth/change`, data);
+};
