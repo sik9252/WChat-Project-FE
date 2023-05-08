159a81c91decbe78ee956188858acf2bf87cf300
sik9252 <sik9252@naver.com>
2023-05-09 01:57:40 +0900

    feat: 공통 레이아웃 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 /** pages */
+import CommonLayout from '../components/CommonLayout';
 import LoginPage from '../pages/LoginPage';
 import RoomsPage from '../pages/RoomsPage';
 import ChatPage from '../pages/ChatPage';
@@ -13,12 +14,16 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<PageNotFound />} />
+        {/* 헤더가 있는 페이지 */}
+        <Route element={<CommonLayout />}>
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/rooms" element={<RoomsPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/chat/:roomId" element={<ChatPage />} />
+          <Route path="/myPage" element={<MyPage />} />
+        </Route>
+        {/* 헤더가 없는 페이지 */}
         <Route path="/" element={<LoginPage />} />
-        <Route path="/rooms" element={<RoomsPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/chat/:roomId" element={<ChatPage />} />
-        <Route path="/myPage" element={<MyPage />} />
         <Route path="/auth/kakao/callback" element={<KakaoLogin />} />
       </Routes>
     </BrowserRouter>
