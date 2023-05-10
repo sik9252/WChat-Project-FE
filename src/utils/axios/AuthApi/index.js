924c75ca7e66815dba4b5da0df139093319d227c
sik9252 <sik9252@naver.com>
2023-05-11 01:48:04 +0900

    feat: 회원 탈퇴 기능 구현

--- src/utils/axios/AuthApi/index.js
+++ src/utils/axios/AuthApi/index.js
@@ -9,3 +9,8 @@ export const kakaoLoginReq = async (code) => {
 export const logoutReq = async () => {
   return await useAxios.post(`/auth/logout`);
 };
+
+/** 회원 탈퇴 */
+export const withDrawlReq = async () => {
+  return await useAxios.delete(`/auth/withdrawal`);
+};
