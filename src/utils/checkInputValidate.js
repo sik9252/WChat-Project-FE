335efbec5124c18be1d3ecdcd6c843cb1fa34e1d
sik9252 <sik9252@naver.com>
2023-05-13 16:16:42 +0900

    feat: 입력값에 특수문자가 포함되었는지 검증하는 함수 추가

---
+++ src/utils/checkInputValidate.js
@@ -0,0 +1,12 @@
+const checkInputValidate = (input) => {
+  // eslint-disable-next-line
+  const regExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
+
+  if (!regExp.test(input)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export default checkInputValidate;
