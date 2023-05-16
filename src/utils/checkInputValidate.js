bf419a72b8bdaec3f63cffe9ffb30e63d0e14e9f
sik9252 <sik9252@naver.com>
2023-05-17 04:06:30 +0900

    fix: 채팅방 생성시 공백 사용 안되는 문제 수정

--- src/utils/checkInputValidate.js
+++ src/utils/checkInputValidate.js
@@ -1,6 +1,6 @@
 const checkInputValidate = (input) => {
   // eslint-disable-next-line
-  const regExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
+  const regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
 
   if (!regExp.test(input)) {
     return true;
